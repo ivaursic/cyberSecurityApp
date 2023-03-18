e680bcfcf57f35fc2be58aeb10316317e0404d24
ivaursic <116602961+ivaursic@users.noreply.github.com>
2023-03-18 10:45:29 +0100

    Add files via upload

---
+++ backend/backend/src/test/java/hr/csa/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.csa.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

37a7eadad7fb3cdd42e8f8ab37448b487d254347
Iva <iva.ursic@fer.hr>
2023-03-18 11:22:19 +0100

    setup

--- backend/backend/src/main/java/hr/csa/backend/BackendApplication.java
+++ backend/backend/src/main/java/hr/csa/backend/BackendApplication.java
@@ -1,10 +1,16 @@
 package hr.csa.backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
+	@Bean
+	public PasswordEncoder pswdEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);

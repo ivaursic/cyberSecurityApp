c4c741cfc8b9723ba228dbd67c79c25e247f9f98
Nikoleta Benić <nb53112@fer.hr>
2023-03-18 11:56:47 +0100

    LoginPage edited

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './components/LoginPage';
+import Header from './components/Header';
+import { Dashboard } from '@mui/icons-material';
 
 function App() {
   return (
     <>
-    <LoginPage></LoginPage>
+    <Header></Header>
     </>
   );
 }

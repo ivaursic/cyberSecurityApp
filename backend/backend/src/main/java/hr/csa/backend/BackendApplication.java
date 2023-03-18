726e9c05c4d70ab5092ba6592b300787daebaefb
Iva <iva.ursic@fer.hr>
2023-03-18 21:36:21 +0100

    random file

--- backend/backend/src/main/java/hr/csa/backend/BackendApplication.java
+++ backend/backend/src/main/java/hr/csa/backend/BackendApplication.java
@@ -1,13 +1,22 @@
 package hr.csa.backend;
 
+import hr.csa.backend.service.ThreatIntelligenceService;
+import hr.csa.backend.service.UserAccountService;
+import hr.csa.backend.service.impl.ThreatIntelligenceServiceJpa;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 @SpringBootApplication
 public class BackendApplication {
+
+	private static ThreatIntelligenceService threatIntelligenceService = new ThreatIntelligenceServiceJpa();
 	@Bean
 	public PasswordEncoder pswdEncoder() {
 		return new BCryptPasswordEncoder();
@@ -15,6 +24,16 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+
+		Timer timer = new Timer();
+
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				threatIntelligenceService.generateRandom();
+			}
+		}, 0, 10*1000);
+
 	}
 
 }

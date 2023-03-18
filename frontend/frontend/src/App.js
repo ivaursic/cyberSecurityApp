7f07e8d935d3d314ac0fa6cebee88972f7236f0d
LDomic05 <lucijadomicsplit@gmail.com>
2023-03-18 16:42:03 +0100

    task/addRegistration

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import LoginPage from './components/LoginPage';
-import Header from './components/Header';
-import { Dashboard } from '@mui/icons-material';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MyAlert from './components/MyAlert';
-import Alerts from './components/MyAlerts'
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import LoginPage from "./components/LoginPage";
+import Header from "./components/Header";
+import Registration from "./components/Registration";
+import { Dashboard } from "@mui/icons-material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MyAlert from "./components/MyAlert";
+import Alerts from "./components/MyAlerts";
 
 function App() {
   return (
     <>
-    <Header/>
-    <><MyAlert></MyAlert></>
-            <Routes>
-                <Route exact path="/" element={<Dashboard></Dashboard>} />
-                <Route exact path="/login" element={<LoginPage></LoginPage>} />
-            </Routes>
-        </>
-
+      <Header />
+      <>
+        <MyAlert></MyAlert>
+      </>
+      <Routes>
+        <Route exact path="/" element={<Dashboard></Dashboard>} />
+        <Route exact path="/login" element={<LoginPage></LoginPage>} />
+        <Route
+          exact
+          path="/registration"
+          element={<Registration></Registration>}
+        />
+      </Routes>
+    </>
   );
 }
 
---
+++ frontend/frontend/src/components/Registration.js
@@ -0,0 +1,103 @@
+import React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
+
+export default function Registration() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+
+    //POSLA NA BACKEND
+  };
+
+  return (
+    // eslint-disable-next-line no-undef
+
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}>
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="Name"
+            name="name"
+            autoComplete="name"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="surname"
+            label="Surname"
+            name="surname"
+            autoComplete="surname"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}>
+            Sign Up
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+}

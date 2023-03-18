6962cbe1d8ac210ecb26fe08945d3be0e4cbd36f
Iva <iva.ursic@fer.hr>
2023-03-18 11:28:58 +0100

    setup2

--- backend/backend/src/main/java/hr/csa/backend/BackendApplication.java
+++ backend/backend/src/main/java/hr/csa/backend/BackendApplication.java
@@ -3,7 +3,8 @@ package hr.csa.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class BackendApplication {
